4e7b8ef84f4738ba89bb17b0c51c82671b0abc08
fabrielg <gfrancois2003@gmail.com>
2025-02-28 12:44:54 +0100

    feat: main: init window with mlx

---
+++ main.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 11:20:02 by gfrancoi          #+#    #+#             */
+/*   Updated: 2025/02/28 12:41:34 by gfrancoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./libft/libft.h"
+#include "./mlx/mlx.h"
+#include "./mlx/mlx_int.h"
+#include <fcntl.h>
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	void	*mlx;
+	void	*window;
+	t_data	img;
+
+	if (ac != 2)
+		return (ft_putendl_fd("Usage: ./fdf <filename>", 1), 0);
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return (ft_putendl_fd("Error: invalid fd", 1), 0);
+	mlx = mlx_init();
+	window = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "FDF");
+	img.img = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+		&img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, window, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
