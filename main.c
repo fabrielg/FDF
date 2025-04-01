789326a386a5f1dd7cbcc1668dce5aad3da97895
Fabrielg <gfrancois2003@gmail.com>
2025-04-02 00:16:14 +0200

    wip: add escape close window hook

--- main.c
+++ main.c
@@ -17,6 +17,19 @@
 #include "matrix.h"
 #include <math.h>
 
+void	close_window(t_fdf *fdf)
+{
+	mlx_destroy_window(fdf->mlx, fdf->window);
+	exit(0);
+}
+
+int	handle_key_pressed(int keycode, t_fdf *fdf)
+{
+	if (keycode == 65307)
+		close_window(fdf);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_fdf	fdf;
@@ -32,13 +45,13 @@ int	main(int ac, char **av)
 		ft_putendl_fd("INIT FDF ERROR", 1);
 		return (0);
 	}
-
 	ft_super_memset(fdf.img.addr, &fdf.img.bg_color, (fdf.img.height) * (fdf.img.width), sizeof(int));
 	close(fd);
 	draw_map(fdf.projected_map, fdf.nb_rows, fdf.nb_cols, &fdf.img);
 	free_map((void **)fdf.origin_map);
 	free_map((void **)fdf.projected_map);
 	mlx_put_image_to_window(fdf.mlx, fdf.window, fdf.img.img, 0, 0);
+	mlx_hook(fdf.window, 2, 1L<<0, handle_key_pressed, &fdf);
 	mlx_loop(fdf.mlx);
 	return (0);
 }
