5e3558ca2d3f520deac381832c9d5f3989571d60
fabrielg <gfrancois2003@gmail.com>
2025-04-23 10:22:50 +0200

    feat: display z_divisor into the menu

--- menu.c
+++ menu.c
@@ -6,7 +6,7 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 20:33:17 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/04/07 16:51:06 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/04/23 10:22:15 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,18 @@ void	put_str(t_fdf *fdf, int x, int y, char *str)
 
 void	display_info(t_fdf *fdf)
 {
+	char	*z_divisor;
+
+	z_divisor = ft_itoa(fdf->projection.z_divisor * 100);
 	put_str(fdf, 30, 50, "=== FDF CONTROLS ===");
 	put_str(fdf, 30, 80, "ESC - QUIT");
+	put_str(fdf, 30, 120, "=== FDF INFOS ===");
+	put_str(fdf, 30, 150, "Z DIVISOR : ");
+	if (z_divisor)
+	{
+		put_str(fdf, 110, 150, z_divisor);
+		free(z_divisor);
+	}
+	else
+		put_str(fdf, 110, 150, "ERROR");
 }
