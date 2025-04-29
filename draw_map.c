abaeac3628f573d8b3c5067e11f25d67830f848d
fabrielg <gfrancois2003@gmail.com>
2025-04-29 21:04:13 +0200

    wip: draw map wired

--- draw_map.c
+++ draw_map.c
@@ -6,13 +6,39 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 19:04:02 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/04/29 19:29:07 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/04/29 21:03:32 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	wire(t_fdf *fdf)
+{
+	t_point2	**projection;
+	int			x;
+	int			y;
+
+	y = 0;
+	projection = fdf->map.projection;
+	while (y < fdf->map.rows)
+	{
+		x = 0;
+		while (x < fdf->map.cols)
+		{
+			if (x + 1 < fdf->map.cols)
+				draw_line(projection[y][x],
+					projection[y][x + 1], &fdf->img_datas);
+			if (y + 1 < fdf->map.rows)
+				draw_line(projection[y][x],
+					projection[y + 1][x], &fdf->img_datas);
+			x++;
+		}	
+		y++;
+	}
+}
+
 void	draw_map(t_fdf *fdf)
 {
 	generate_background(fdf);
+	wire(fdf);
 }
