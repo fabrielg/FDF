79e2689158099eb5d0d79175511f480465d4b589
fabrielg <gfrancois2003@gmail.com>
2025-05-05 21:29:52 +0200

    feat: can increase and downcrease z_coeff

--- control_keys.c
+++ control_keys.c
@@ -6,21 +6,25 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 20:20:12 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/05/05 21:13:53 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:28:50 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "keycodes.h"
 
-int	key_press(int keycode, t_fdf *fdf)
+int	key_press(int k, t_fdf *fdf)
 {
-	if (keycode == KEY_ESC)
+	if (k == KEY_ESC)
 		close_window(fdf);
-	else if (keycode == KEY_RIGHT)
+	else if (k == KEY_RIGHT)
 		fdf->map.camera[Z] += 0.1f;
-	else if (keycode == KEY_LEFT)
+	else if (k == KEY_LEFT)
 		fdf->map.camera[Z] -= 0.1f;
+	else if ((k == KEY_SUM || k == KEY_SUM2) && fdf->map.z_coeff < 0.5f)
+		fdf->map.z_coeff += 0.01f;
+	else if ((k == KEY_DIF || k == KEY_DIF2) && fdf->map.z_coeff > 0.01f)
+		fdf->map.z_coeff -= 0.01f;
 	else
 		return (0);
 	draw_map(fdf);
